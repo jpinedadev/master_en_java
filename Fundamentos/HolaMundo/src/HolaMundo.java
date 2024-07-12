c661cf7c2426a5f653941cdce3890a3664996021
jpinedadev <104587810+jpinedadev@users.noreply.github.com>
2024-07-12 16:06:19 -0500

    iniciando el curso
    Iniciando proyectos

---
+++ Fundamentos/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,21 @@
+
+public class HolaMundo {
+    public static void main(String args[]){
+            int miVariableEntera = 10;
+            System.out.println(miVariableEntera);
+            miVariableEntera = 5;
+            System.out.println(miVariableEntera);
+            
+            String miVariableCadena = "Saludos";
+            System.out.println(miVariableCadena);
+            miVariableCadena = "Adios";
+            System.out.println(miVariableCadena);
+            
+            var miVariableEntera2 = 15;
+            System.out.println(miVariableEntera2);
+            
+            var miVariableCadena2 = "hola";
+            System.out.println(miVariableCadena2);
+            
+    }
+}
