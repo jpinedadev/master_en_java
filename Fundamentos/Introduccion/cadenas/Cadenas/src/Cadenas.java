e94324f839b2d1f88abf857c7beced7be8cf547a
jpinedadev <104587810+jpinedadev@users.noreply.github.com>
2024-07-26 19:11:11 -0500

    cadenas
    cadenas en java

---
+++ Fundamentos/Introduccion/cadenas/Cadenas/src/Cadenas.java
@@ -0,0 +1,5 @@
+public class Cadenas {
+    public static void main(String[] args) {
+        System.out.println("*** Manejo de cadenas ***");
+    }
+}
---
+++ Fundamentos/Introduccion/cadenas/Cadenas/src/IndicesCadena.java
@@ -0,0 +1,8 @@
+public class IndicesCadena {
+    public static void main(String[] args) {
+        var cadena1 = "Hola Mundo";
+        System.out.println("cadena1 = " + cadena1);
+        var miPrimerCaracter = cadena1.charAt(5);
+        System.out.println("miPrimerCaracter = " + miPrimerCaracter);
+    }
+}
